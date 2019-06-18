b5cab96634dd9dd6e12d6c28f1f9c0f8bead7654
xiaotiangege <361807535@qq.com>
2019-06-18 15:08:46 +0800

    ""

--- src/main/java/com/xiaotiangege/web/frame/annotation/Interceptor.java
+++ src/main/java/com/xiaotiangege/web/frame/annotation/Interceptor.java
@@ -1,11 +1,20 @@
 package com.xiaotiangege.web.frame.annotation;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @author 小天哥哥 
  * @mailto 361807535@qq.com  
  * @Description TODO 
  * @createDate 2019年6月18日 下午2:14:17 
  */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
 public @interface Interceptor {
 
 }
--- src/main/java/com/xiaotiangege/web/frame/annotation/UrlMapping.java
+++ src/main/java/com/xiaotiangege/web/frame/annotation/UrlMapping.java
@@ -1,11 +1,39 @@
 package com.xiaotiangege.web.frame.annotation;
 
-/**
- * @author 小天哥哥 
- * @mailto 361807535@qq.com  
- * @Description TODO 
- * @createDate 2019年6月18日 下午2:13:34 
- */
-public @interface UrlMapping {
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+/**
+ * @author 小天哥哥
+ * @mailto 361807535@qq.com
+ * @Description TODO
+ * @createDate 2019年6月18日 下午2:13:34
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE, ElementType.METHOD })
+public @interface UrlMapping {
+	/**
+	 * 请求地址
+	 * 
+	 * @return
+	 */
+	String value();
+
+	/**
+	 * 请求地址
+	 * 
+	 * @return
+	 */
+	String uri() default "";
+
+	/**
+	 * 请求描述
+	 * 
+	 * @return
+	 */
+	String desc() default "";
 }
